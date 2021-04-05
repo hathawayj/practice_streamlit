060fac478db5259c23aeaa45041b635ea49c66b3
J. Hathaway <hathawayj@gmail.com>
2021-04-05 12:25:37 -0600

    first draft

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# practice_streamlit
+# STREAMLIT: The data science dashboard tool
+
+## Installation
+
+Streamlit has guidance on [how to clean install Streamlit](https://docs.streamlit.io/en/stable/troubleshooting/clean-install.html) that works. I have tested the Mac installation process.
+
+## References
+
+- https://towardsdatascience.com/how-to-deploy-a-semantic-search-engine-with-streamlit-and-docker-on-aws-elastic-beanstalk-42ddce0422f3
+- https://discuss.streamlit.io/t/how-to-use-streamlit-in-docker/1067
+- https://towardsdatascience.com/coding-ml-tools-like-you-code-ml-models-ddba3357eace
+- https://towardsdatascience.com/quickly-build-and-deploy-an-application-with-streamlit-988ca08c7e83
--- practice.py
+++ practice.py
@@ -1,19 +1,51 @@
 # %%
+# import sys
+# !{sys.executable} -m pip install streamlit
+# %%
 import streamlit as st 
 import pandas as pd 
 import altair as alt 
 import numpy as np 
 
 # %%
+
+st.set_page_config(page_title="Temples of the Restoration")
+
+
 st.write("""
 # Temples of the World
 
-A test project using Python, Streamlit, and Altair
+A visual project using Python, Streamlit, and Altair
 
 _J. Hathaway_
 
 """)
 
-dat = pd.read_csv('temples.csv')
-st.line_chart(dat)
+@st.cache
+def load_data():
+    df = (pd.read_csv('temples.csv')
+        .query('not SquareFootage.isnull()')
+        .filter(['Temple', 'Country', 'State', 'City', 
+            'SquareFootage', 'InstructionRooms', 'SealingRooms',
+            'BaptismRooms', 'Acreage', 'Latitude', 'Longitude', 
+            'Announcement', 'Groundbreaking', 'Dedication']))
+    return df
+
+dat = load_data()
+cnames = dat.Country.sort_values(ascending=False).unique().tolist()
+
+st.write(dat.style.set_precision(0))
+
+age = st.sidebar.slider("Choose your Birth Year: ", min_value=1930,
+    max_value=2020, value=2000, step = 1)
+
+country = st.sidebar.multiselect("Where were you born?", 
+                         cnames)
 # %%
+c = (alt.Chart(dat)
+    .encode(alt.X('Announcement:T'), 
+        alt.Y('SquareFootage'))
+    .mark_circle()
+    .interactive())
+
+st.altair_chart(c, use_container_width=True)
