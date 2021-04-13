0106f801855444d25dbbe3f78d953234797aa102
J. Hathaway <hathawayj@gmail.com>
2021-04-12 21:37:59 -0600

    third try

--- setup.sh
+++ setup.sh
@@ -1,13 +1,12 @@
-mkdir -p ~/.streamlit/
+mkdir -p ~/.streamlit
 
 echo "\
 [general]\n\
 email = \"hathawayj@byui.edu\"\n\
 " > ~/.streamlit/credentials.toml
 
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo "[server]
+headless = true
+enableCORS=false
+port = $PORT
 " > ~/.streamlit/config.toml
